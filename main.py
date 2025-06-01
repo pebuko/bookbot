cea2e89401be3122202377bd90925bc049ab9003
pebuko <peterbusfy@seznam.cz>
2025-06-01 21:14:31 +0200

    first func

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_text = f.read()
+    return file_text
+relpath = "books/frankenstein.txt"
+def main(relpath):
+    get_book_text(relpath)
+    print(relpath)
+main()
+
+
