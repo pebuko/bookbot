8347f80357f9be4942f76570881a19e430d0de70
pebuko <peterbusfy@seznam.cz>
2025-06-01 21:30:17 +0200

    fix main

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ def get_book_text(filepath):
     with open(filepath) as f:
         file_text = f.read()
     return file_text
-relpath = "books/frankenstein.txt"
-def main(relpath):
+
+def main():
+    relpath = "books/frankenstein.txt"
     result = get_book_text(relpath)
     print(result)
+
+
 main()
 
 
