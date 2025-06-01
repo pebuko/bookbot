000a5264895c4b1012ab42bba398252ca25d417d
pebuko <peterbusfy@seznam.cz>
2025-06-01 21:55:39 +0200

    implement wordcount

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ def get_book_text(filepath):
         file_text = f.read()
     return file_text
 
+def get_word_count(text):
+    words = text.split()
+    return len(words)
+
 def main():
     relpath = "books/frankenstein.txt"
     result = get_book_text(relpath)
-    print(result)
-
+    word_count = get_word_count(result)
+    print (f"{word_count} words found in the document")
 
 main()
 
