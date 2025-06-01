651fb750507cafbc885bd956c5769b13f924227e
pebuko <peterbusfy@seznam.cz>
2025-06-01 22:17:15 +0200

    letter stats added

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
+from stats import get_word_count
+from stats import text_stats
+
+
 def get_book_text(filepath):
     with open(filepath) as f:
         file_text = f.read()
     return file_text
 
-def get_word_count(text):
-    words = text.split()
-    return len(words)
+
 
 def main():
     relpath = "books/frankenstein.txt"
     result = get_book_text(relpath)
     word_count = get_word_count(result)
     print (f"{word_count} words found in the document")
-
+    stats = text_stats(result)
+    print(stats)
 main()
 
 
